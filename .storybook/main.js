27ce1b807a3f92fd4657488b9af656411e25ad8b
Raed Chammam <raed.chammam@ricardo.ch>
2021-02-17 15:46:06 +0100

    Add storybook

---
+++ .storybook/main.js
@@ -0,0 +1,9 @@
+module.exports = {
+  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: [
+    '@storybook/addon-knobs',
+    '@storybook/addon-actions',
+    '@storybook/addon-essentials',
+    '@storybook/addon-links',
+  ],
+}
