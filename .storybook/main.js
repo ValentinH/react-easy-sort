38f7b94d188b5cf0fc89aceaa168be7a092adf43
Jan-Vermeulen <jan.vermeulen@ricardo.ch>
2021-09-06 20:09:46 +0200

    update storybook and use controls instead of knobs

--- .storybook/main.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: [
-    '@storybook/addon-knobs',
-    '@storybook/addon-actions',
-    '@storybook/addon-essentials',
-    '@storybook/addon-links',
-  ],
-}
--- src/stories/simple-vertical-list/index.stories.tsx
+++ src/stories/simple-vertical-list/index.stories.tsx
@@ -1,17 +1,31 @@
 import React from 'react'
 import arrayMove from 'array-move'
 
-import { withKnobs, number } from '@storybook/addon-knobs'
 import { action } from '@storybook/addon-actions'
+import { Story } from '@storybook/react'
 
 import SortableList, { SortableItem } from '../../index'
 import { generateItems } from '../helpers'
 import { makeStyles } from '@material-ui/core'
 
 export default {
-  title: 'react-easy-sort/Simple vertical list',
   component: SortableList,
-  decorators: [withKnobs],
+  title: 'react-easy-sort/Simple vertical list',
+  parameters: {
+    componentSubtitle: 'SortableList',
+  },
+  argTypes: {
+    count: {
+      name: 'Number of elements',
+      control: {
+        type: 'range',
+        min: 3,
+        max: 12,
+        step: 1,
+      },
+      defaultValue: 3,
+    },
+  },
 }
 
 const useStyles = makeStyles({
@@ -36,9 +50,12 @@ const useStyles = makeStyles({
   },
 })
 
-export const Demo = () => {
+type StoryProps = {
+  count: number
+}
+
+export const Demo: Story<StoryProps> = ({ count }: StoryProps) => {
   const classes = useStyles()
-  const count = number('Items', 9, { min: 3, max: 12, range: true })
 
   const [items, setItems] = React.useState<string[]>([])
   React.useEffect(() => {
